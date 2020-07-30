c9d6784e6ea2ec0ed652eec633d0b45e3499a809
irinchick <sinnosence@gmail.com>
2020-07-30 23:22:14 +0200

    cleaning up the code

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
-import Login from './components/Login'
-import MealPlans from './components/MealPlans'
-import MealPlanDetail from './components/MealPlanDetail'
-import { GET_SCHEMA} from './api/graphql'
+import MealPlans from './components/MealPlans';
+import MealPlanDetail from './components/MealPlanDetail';
+import { GET_SCHEMA} from './api/graphql';
 import { useQuery } from '@apollo/client';
 
 function App() {
-  const loggedIn = localStorage.getItem('token') ? true : false;
-  const { loading, error, data, refetch, updateQuery } = useQuery(GET_SCHEMA);
+ // useQuery(GET_SCHEMA);
   return (
     <div className="App">
       <Router>
@@ -24,7 +22,7 @@ function App() {
                 <MealPlanDetail />
             </Route>
             <Route path="/">
-              {loggedIn ? <MealPlans /> : <Login />}
+               <MealPlans /> 
             </Route>
           </Switch>
       </Router>
